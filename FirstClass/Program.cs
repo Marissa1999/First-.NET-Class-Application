2aa9fba3cac716ea216c3ad713d2f02e7bddaafa
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 14:39:49 -0500

    Initial Commit.

---
+++ FirstClass/Program.cs
@@ -0,0 +1,22 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Lab 1 - Example 1: How to set up a project in C#
+
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is my first C# program.");
+
+        }
+    }
+}
